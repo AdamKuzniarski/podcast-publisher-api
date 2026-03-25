b6bd352763f7835a845c27f4a4bcd3d9c0ae25cc
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-03-25 21:54:36 +0100

    initial commit

---
+++ src/main/java/io/github/adamkuzniarski/podcastpublisherapi/PodcastPublisherApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.adamkuzniarski.podcastpublisherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastPublisherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodcastPublisherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/adamkuzniarski/podcastpublisherapi/PodcastPublisherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adamkuzniarski.podcastpublisherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastPublisherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
