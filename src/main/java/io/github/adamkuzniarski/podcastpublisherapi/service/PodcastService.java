999a8b6102b0d9ffd39c4f77ef9c9c20909edcf5
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-03-27 22:36:52 +0100

    add PodcastService.java with dummy data

---
+++ src/main/java/io/github/adamkuzniarski/podcastpublisherapi/service/PodcastService.java
@@ -0,0 +1,16 @@
+package io.github.adamkuzniarski.podcastpublisherapi.service;
+
+import io.github.adamkuzniarski.podcastpublisherapi.dto.PodcastResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PodcastService {
+
+    public List<PodcastResponse> getPodcasts() {
+        return List.of(
+                new PodcastResponse(1L, "Java Backend Basics","Ein Podcast über Backend-Lernen","Adam")
+        );
+    }
+}
