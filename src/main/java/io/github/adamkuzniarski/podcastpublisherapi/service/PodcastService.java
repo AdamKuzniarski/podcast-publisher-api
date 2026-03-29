1878592da8e97bf84581fdf658b686529e265233
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-03-29 16:55:28 +0200

    add a new method in PodcastService.java - createPodcast

--- src/main/java/io/github/adamkuzniarski/podcastpublisherapi/service/PodcastService.java
+++ src/main/java/io/github/adamkuzniarski/podcastpublisherapi/service/PodcastService.java
@@ -1,5 +1,6 @@
 package io.github.adamkuzniarski.podcastpublisherapi.service;
 
+import io.github.adamkuzniarski.podcastpublisherapi.dto.CreatePodcastRequest;
 import io.github.adamkuzniarski.podcastpublisherapi.dto.PodcastResponse;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,15 @@ public class PodcastService {
         return List.of(
                 new PodcastResponse(1L, "Java Backend Basics","Ein Podcast über Backend-Lernen","Adam"),
                 new PodcastResponse(2L, "Spring Boot Insights","Ein Podcast über Spring Boot","Adam")
+        );
+    }
+
+    public PodcastResponse createPodcast(CreatePodcastRequest request){
+        return new PodcastResponse(
+                3L,
+                request.title(),
+                request.description(),
+                request.author()
         );
     }
 }
