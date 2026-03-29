4bd32eb63212ca3f340654ee3270901c007af519
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-03-30 00:04:56 +0200

    add ArrayList storage for imitation of DB

--- src/main/java/io/github/adamkuzniarski/podcastpublisherapi/service/PodcastService.java
+++ src/main/java/io/github/adamkuzniarski/podcastpublisherapi/service/PodcastService.java
@@ -4,24 +4,33 @@ import io.github.adamkuzniarski.podcastpublisherapi.dto.CreatePodcastRequest;
 import io.github.adamkuzniarski.podcastpublisherapi.dto.PodcastResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PodcastService {
 
-    public List<PodcastResponse> getPodcasts() {
-        return List.of(
-                new PodcastResponse(1L, "Java Backend Basics","Ein Podcast über Backend-Lernen","Adam"),
-                new PodcastResponse(2L, "Spring Boot Insights","Ein Podcast über Spring Boot","Adam")
-        );
+    private final List<PodcastResponse> podcasts = new ArrayList<>();
+
+    public PodcastService() {
+        podcasts.add(new PodcastResponse(1L, "Java Backend Basics", "Ein Podcast über Backend-Lernen", "Adam"));
+        podcasts.add(new PodcastResponse(2L, "Spring Boot Insights", "Ein Podcast über Spring
     }
 
-    public PodcastResponse createPodcast(CreatePodcastRequest request){
-        return new PodcastResponse(
-                3L,
+    public List<PodcastResponse> getPodcasts() {
+        return podcasts;
+    }
+
+    public PodcastResponse createPodcast(CreatePodcastRequest request) {
+        Long newId = (long) (podcasts.size() + 1);
+
+        PodcastResponse newPodcast = new PodcastResponse(
+                newId,
                 request.title(),
                 request.description(),
                 request.author()
         );
+        podcasts.add(newPodcast);
+        return newPodcast;
     }
 }
