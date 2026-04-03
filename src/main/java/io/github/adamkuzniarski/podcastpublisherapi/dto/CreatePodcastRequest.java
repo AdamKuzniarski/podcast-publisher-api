5894a48740c8949ecf7fc9275260bd2ffc3f3fb6
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-04-03 14:51:03 +0200

    add jakarta.validation to CreatePodcastRequest.java record with @NotBlank and @Size Annotations.

--- src/main/java/io/github/adamkuzniarski/podcastpublisherapi/dto/CreatePodcastRequest.java
+++ src/main/java/io/github/adamkuzniarski/podcastpublisherapi/dto/CreatePodcastRequest.java
@@ -1,8 +1,19 @@
 package io.github.adamkuzniarski.podcastpublisherapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreatePodcastRequest(
-        String title,
-        String description,
-        String author
+@NotBlank(message = "Title must not be blank")
+@Size(max = 100, message = "Title must not exceed 100 characters")
+String title,
+
+@NotBlank(message = "Description must not be blank")
+@Size(max = 500, message = "Descriptoion must not exceed 500 characters")
+String description,
+
+@NotBlank(message = "Author must not be blank")
+@Size(max = 50, message = "Author must not exceed 50 characters")
+String author
 ) {
 }
